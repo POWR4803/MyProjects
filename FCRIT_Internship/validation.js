4124d52c510ac7fbcf80aa595183a96b5dca460e
shreyash <shreyash4803@gmail.com>
2022-12-26 20:29:43 +0530

    project completed

--- FCRIT_Internship/validation.js
+++ FCRIT_Internship/validation.js
@@ -1,6 +1,7 @@
 function validate(){
     var msg = "Please enter all the Details."
-    if(document.getElementById('name').value ==""){
+    if(document.getElementById('name').value =="" ||document.getElementById('branch').value =="" ||document.getElementById('Sem').value =="" ||
+    document.getElementById('rno').value =="" ||document.getElementById('pno').value =="" ||document.getElementById('email').value ==""){
      alert(msg);
      return false;
     }
